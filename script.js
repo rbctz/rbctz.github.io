38f347c6147ddbb909703d7f58c46136a1393490
Robert Bacula <bacularobert@gmail.com>
2025-10-08 14:42:49 +0300

    test

--- script.js
+++ script.js
@@ -146,40 +146,6 @@ window.addEventListener("load", () => {
   });
 })();
 
-// Clock
-
-const time = document.getElementById("time");
-const day = document.getElementById("day");
-
-let clock = setInterval(
-  function calcTime() {
-    let date_now = new Date();
-    let hr = date_now.getHours();
-    let min = date_now.getMinutes();
-    let sec = date_now.getSeconds();
-
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-
-    day.textContent = days[date_now.getDay()];
-
-    hr = hr < 10 ? "0" + hr : hr;
-    min = min < 10 ? "0" + min : min;
-    sec = sec < 10 ? "0" + sec : sec;
-
-    time.textContent = hr + ":" + min + ":" + sec;
-  },
-
-  1000
-);
-
 // Page load animations
 window.addEventListener("load", () => {
   document.querySelectorAll("section").forEach((section) => {
